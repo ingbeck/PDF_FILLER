feba8a7150c5d6265381b25be6f72d572e079cce
ingbeck <156573301+ingbeck@users.noreply.github.com>
2025-08-26 09:28:24 +0200

    initial commit

---
+++ src/main/java/com/github/ingbeck/kuduze_pdf_agent/KuduzePdfAgentApplication.java
@@ -0,0 +1,13 @@
+package com.github.ingbeck.kuduze_pdf_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuduzePdfAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuduzePdfAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ingbeck/kuduze_pdf_agent/KuduzePdfAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ingbeck.kuduze_pdf_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuduzePdfAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
